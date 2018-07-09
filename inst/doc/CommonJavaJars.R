195261e16f9d917b762793c89e2a0fddf7029f73
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2018-07-09 12:30:05 +0000

    version 1.0-6

--- inst/doc/CommonJavaJars.R
+++ inst/doc/CommonJavaJars.R
@@ -1,45 +1,31 @@
-### R code from vignette source 'CommonJavaJars.Rnw'
-
-###################################################
-### code chunk number 1: CommonJavaJars.Rnw:31-36
-###################################################
+## ----echo=FALSE,results='hide'------------------------------------------------------------------------------------------------------------
 
 options(width=140)
 options(prompt=" ", continue=" ")
 options(digits=4)
 
 
+## ----echo=TRUE,eval=F---------------------------------------------------------------------------------------------------------------------
+#  
+#  .jpackage("CommonJavaJars", jars=c("forms-1.2.0.jar", "iText-2.1.4.jar"))
+#  
 
-###################################################
-### code chunk number 2: CommonJavaJars.Rnw:47-50 (eval = FALSE)
-###################################################
-## 
-## .jpackage("CommonJavaJars", jars=c("forms-1.2.0.jar", "iText-2.1.4.jar"))
-## 
+## ----echo=TRUE,eval=F---------------------------------------------------------------------------------------------------------------------
+#  
+#  loadJars(c("forms", "iText"))
+#  
 
-
-###################################################
-### code chunk number 3: CommonJavaJars.Rnw:58-61 (eval = FALSE)
-###################################################
-## 
-## loadJars(c("forms", "iText"))
-## 
-
-
-###################################################
-### code chunk number 4: CommonJavaJars.Rnw:90-102 (eval = FALSE)
-###################################################
-## # The following few lines are based on the code of the rJava .jpackage function
-## if (!is.null(sessionInfo()$otherPkgs$rJava$Version) && sessionInfo()$otherPkgs$rJava$Version < "0.8-3") {
-## 	classes <- system.file("R28", package = "CommonJavaJars", lib.loc = NULL)
-## 	if (nchar(classes)) {
-## 		.jaddClassPath(classes)
-## 		jars <- grep(".*\\.jar", list.files(classes, full.names = TRUE), TRUE, value = TRUE)
-## 		if (length(jars)) { 
-## 			.jaddClassPath(jars)
-## 		}		
-## 	}
-## }
-## # Otherwise load from rJava. 
-
+## ----echo=TRUE,eval=FALSE-----------------------------------------------------------------------------------------------------------------
+#  # The following few lines are based on the code of the rJava .jpackage function
+#  if (!is.null(sessionInfo()$otherPkgs$rJava$Version) && sessionInfo()$otherPkgs$rJava$Version < "0.8-3") {
+#  	classes <- system.file("R28", package = "CommonJavaJars", lib.loc = NULL)
+#  	if (nchar(classes)) {
+#  		.jaddClassPath(classes)
+#  		jars <- grep(".*\\.jar", list.files(classes, full.names = TRUE), TRUE, value = TRUE)
+#  		if (length(jars)) {
+#  			.jaddClassPath(jars)
+#  		}		
+#  	}
+#  }
+#  # Otherwise load from rJava.
 
